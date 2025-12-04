3f7b353ca0b1ffed815ff84d9689466486158e03
borgo02 <paolo.roselli02@gmail.com>
2025-12-04 17:14:08 +0100

    missing packages and move to config.yml

--- args.py
+++ args.py
@@ -1,105 +1,93 @@
+import argparse
+import yaml
+import os
 
 ### program configuration
 class Args():
     def __init__(self):
-        ### if clean tensorboard
-        self.clean_tensorboard = False
-        ### Which CUDA GPU device is used for training
-        self.cuda = False
+        # 1. Load config from file
+        config = {}
+        if os.path.exists('config.yaml'):
+            with open('config.yaml', 'r') as f:
+                config = yaml.safe_load(f)
 
-        ### Which GraphRNN model variant is used.
-        # The simple version of Graph RNN
-        # self.note = 'GraphRNN_MLP'
-        # The dependent Bernoulli sequence version of GraphRNN
-        self.note = 'GraphRNN_RNN'
+        # 2. Define CLI args (defaults come from config or hardcoded fallback)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--config', type=str, default='config.yaml', help='Path to config file')
+        parser.add_argument('--graph_type', type=str, default=config.get('graph_type', 'grid'), help='Graph type')
+        parser.add_argument('--epochs', type=int, default=config.get('epochs', 3000), help='Number of epochs')
+        parser.add_argument('--cuda', action='store_true', default=config.get('cuda', False), help='Use CUDA')
+        parser.add_argument('--note', type=str, default=config.get('note', 'GraphRNN_RNN'), help='Note')
+        
+        # Parse args
+        args, unknown = parser.parse_known_args()
+        
+        # Reload config if a different config file was specified via CLI
+        if args.config != 'config.yaml' and os.path.exists(args.config):
+             with open(args.config, 'r') as f:
+                config = yaml.safe_load(f)
+                # Re-parse args to ensure CLI overrides new config defaults if necessary
+                # (Simple approach: just trust CLI args > config file values for explicitly passed args)
 
-        ## for comparison, removing the BFS compoenent
-        # self.note = 'GraphRNN_MLP_nobfs'
-        # self.note = 'GraphRNN_RNN_nobfs'
+        # 3. Set attributes (Priority: CLI > Config > Defaults)
+        # We use the parsed args directly for things exposed in CLI
+        self.graph_type = args.graph_type
+        self.epochs = args.epochs
+        self.cuda = args.cuda
+        self.note = args.note
+        self.clean_tensorboard = config.get('clean_tensorboard', False)
 
-        ### Which dataset is used to train the model
-        # self.graph_type = 'DD'
-        # self.graph_type = 'caveman'
-        # self.graph_type = 'caveman_small'
-        # self.graph_type = 'caveman_small_single'
-        # self.graph_type = 'community4'
-        self.graph_type = 'grid'
-        # self.graph_type = 'grid_small'
-        # self.graph_type = 'ladder_small'
-
-        # self.graph_type = 'enzymes'
-        # self.graph_type = 'enzymes_small'
-        # self.graph_type = 'barabasi'
-        # self.graph_type = 'barabasi_small'
-        # self.graph_type = 'citeseer'
-        # self.graph_type = 'citeseer_small'
-
-        # self.graph_type = 'barabasi_noise'
-        # self.noise = 10
-        #
-        # if self.graph_type == 'barabasi_noise':
-        #     self.graph_type = self.graph_type+str(self.noise)
-
-        # if none, then auto calculate
-        self.max_num_node = None # max number of nodes in a graph
-        self.max_prev_node = None # max previous node that looks back
-
-        ### network config
-        ## GraphRNN
+        # 4. Set other attributes from config (or defaults if missing)
+        self.max_num_node = config.get('max_num_node', None)
+        self.max_prev_node = config.get('max_prev_node', None)
+        
+        self.hidden_size_rnn = config.get('hidden_size_rnn', 128)
+        self.hidden_size_rnn_output = config.get('hidden_size_rnn_output', 16)
+        self.embedding_size_rnn = config.get('embedding_size_rnn', 64)
+        self.embedding_size_rnn_output = config.get('embedding_size_rnn_output', 8)
+        self.embedding_size_output = config.get('embedding_size_output', 64)
+        self.num_layers = config.get('num_layers', 4)
+        
+        # Adjust sizes based on graph type (legacy logic)
         if 'small' in self.graph_type:
             self.parameter_shrink = 2
         else:
             self.parameter_shrink = 1
-        self.hidden_size_rnn = int(128/self.parameter_shrink) # hidden size for main RNN
-        self.hidden_size_rnn_output = 16 # hidden size for output RNN
-        self.embedding_size_rnn = int(64/self.parameter_shrink) # the size for LSTM input
-        self.embedding_size_rnn_output = 8 # the embedding size for output rnn
-        self.embedding_size_output = int(64/self.parameter_shrink) # the embedding size for output (VAE/MLP)
+            
+        self.hidden_size_rnn = int(self.hidden_size_rnn/self.parameter_shrink)
+        self.embedding_size_rnn = int(self.embedding_size_rnn/self.parameter_shrink)
+        self.embedding_size_output = int(self.embedding_size_output/self.parameter_shrink)
 
-        self.batch_size = 32 # normal: 32, and the rest should be changed accordingly
-        self.test_batch_size = 32
-        self.test_total_size = 1000
-        self.num_layers = 4
-
-        ### training config
-        self.num_workers = 4 # num workers to load data, default 4
-        self.batch_ratio = 32 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
-        self.epochs = 3000 # now one epoch means self.batch_ratio x batch_size
-        self.epochs_test_start = 100
-        self.epochs_test = 100
-        self.epochs_log = 100
-        self.epochs_save = 100
-
-        self.lr = 0.003
-        self.milestones = [400, 1000]
-        self.lr_rate = 0.3
-
-        self.sample_time = 2 # sample time in each time step, when validating
-
-        ### output config
-        # self.dir_input = "/dfs/scratch0/jiaxuany0/"
-        self.dir_input = "./"
-        self.model_save_path = self.dir_input+'model_save/' # only for nll evaluation
-        self.graph_save_path = self.dir_input+'graphs/'
-        self.figure_save_path = self.dir_input+'figures/'
-        self.timing_save_path = self.dir_input+'timing/'
-        self.figure_prediction_save_path = self.dir_input+'figures_prediction/'
-        self.nll_save_path = self.dir_input+'nll/'
-
-
-        self.load = False # if load model, default lr is very low
-        self.load_epoch = 3000
-        self.save = True
-
-
-        ### baseline config
-        # self.generator_baseline = 'Gnp'
-        self.generator_baseline = 'BA'
-
-        # self.metric_baseline = 'general'
-        # self.metric_baseline = 'degree'
-        self.metric_baseline = 'clustering'
-
+        self.batch_size = config.get('batch_size', 32)
+        self.test_batch_size = config.get('test_batch_size', 32)
+        self.test_total_size = config.get('test_total_size', 1000)
+        
+        self.num_workers = config.get('num_workers', 4)
+        self.batch_ratio = config.get('batch_ratio', 32)
+        self.epochs_test_start = config.get('epochs_test_start', 100)
+        self.epochs_test = config.get('epochs_test', 100)
+        self.epochs_log = config.get('epochs_log', 100)
+        self.epochs_save = config.get('epochs_save', 100)
+        
+        self.lr = config.get('lr', 0.003)
+        self.milestones = config.get('milestones', [400, 1000])
+        self.lr_rate = config.get('lr_rate', 0.3)
+        self.sample_time = config.get('sample_time', 2)
+        
+        self.dir_input = config.get('dir_input', "./")
+        self.model_save_path = self.dir_input + 'model_save/'
+        self.graph_save_path = self.dir_input + 'graphs/'
+        self.figure_save_path = self.dir_input + 'figures/'
+        self.timing_save_path = self.dir_input + 'timing/'
+        self.figure_prediction_save_path = self.dir_input + 'figures_prediction/'
+        self.nll_save_path = self.dir_input + 'nll/'
+        
+        self.load = config.get('load', False)
+        self.load_epoch = config.get('load_epoch', 3000)
+        self.save = config.get('save', True)
+        
+        self.generator_baseline = config.get('generator_baseline', 'BA')
+        self.metric_baseline = config.get('metric_baseline', 'clustering')
 
         ### filenames to save intemediate and final outputs
         self.fname = self.note + '_' + self.graph_type + '_' + str(self.num_layers) + '_' + str(self.hidden_size_rnn) + '_'
